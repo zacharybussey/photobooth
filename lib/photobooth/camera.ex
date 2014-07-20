6357efb474126ffa85ed23d2d5faee556c838e36
Zach Bussey <zacharybussey@gmail.com>
2014-07-20 14:47:48 -0400

    Removed spawn

--- lib/photobooth/camera.ex
+++ lib/photobooth/camera.ex
@@ -2,7 +2,7 @@ defmodule Photobooth.Camera do
 
 	def snap_image do
 		#System.cmd "gphoto2", ["--capture-image"]
-		spawn fn -> :os.cmd 'gphoto2 --capture-image' end
+		:os.cmd 'gphoto2 --capture-image'
 	end
 	
 end
