61c5bf39f57d8610edeabe2d1dc9b03ddb705d5d
Zach Bussey <zacharybussey@gmail.com>
2014-08-03 23:40:37 -0400

    Added missind files.

---
+++ lib/photobooth/main.ex
@@ -0,0 +1,31 @@
+defmodule Photobooth.Main do
+	
+	def process(:help) do
+		IO.puts """
+		usage photobooth --mode
+		"""
+
+		System.halt(0)
+	end
+
+	def process(:snap) do
+		Photobooth.Camera.snap_image
+		System.halt(0)
+	end
+
+	def process(:set) do
+		current_image = Photobooth.Camera.snap_set
+		if current_image < 4 do
+			:timer.sleep 10000
+			process :set
+		end
+	end
+
+	def process(:booth) do
+		process :set
+		Photobooth.Image.make_folder |> 
+		Photobooth.Camera.download_images |>
+		Photobooth.Image.montage
+		Photobooth.Camera.delete_images
+	end
+end
