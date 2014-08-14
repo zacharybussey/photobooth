b1623bde97425c0f64b3099b115e2ef43ab9ed26
Zach Bussey <zacharybussey@gmail.com>
2014-08-13 23:57:15 -0400

    Sleep times.

--- lib/photobooth/main.ex
+++ lib/photobooth/main.ex
@@ -16,14 +16,14 @@ defmodule Photobooth.Main do
 	def process(:set) do
 		current_image = Photobooth.Camera.snap_set
 		if current_image < 4 do
-			:timer.sleep 10000
+			:timer.sleep 5000
 			process :set
 		end
 	end
 
 	def process(:booth) do
 		process :set
-		:timer.sleep 1000 # Wait for the camera to finish saving the last photo.
+		:timer.sleep 2000 # Wait for the camera to finish saving the last photo.
 		Photobooth.Image.make_folder |> 
 		Photobooth.Camera.download_images |>
 		Photobooth.Image.montage
