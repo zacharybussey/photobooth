56f1d8aa302b7f09f3fd9f6f88431b2f908a5f80
Zach Bussey <zacharybussey@gmail.com>
2014-08-14 22:00:34 -0400

    Reworking.

--- lib/photobooth/pins.ex
+++ lib/photobooth/pins.ex
@@ -1,8 +1,16 @@
 defmodule Photobooth.Pins do
 	use GenServer
 
+	defmodule State do
+		defstruct led1: nil, led2: nil, led3: nil, led4: nil, shutter_button: nil, busy: false
+	end
+
 	def start_link() do
 		GenServer.start_link(__MODULE__, [], name: __MODULE__)
+	end
+
+	def countdown() do
+		GenServer.call __MODULE__, :countdown
 	end
 
 	def finished_processing() do
@@ -12,32 +20,61 @@ defmodule Photobooth.Pins do
 	def init([]) do
 		Gpio.start_link 17, :input #Access exception
 		{:ok, shutter_pid} = Gpio.start_link 17, :input
+		{:ok, led1_pid} = Gpio.start_link 21, :output
+		{:ok, led2_pid} = Gpio.start_link 22, :output
+		{:ok, led3_pid} = Gpio.start_link 23, :output
+		{:ok, led4_pid} = Gpio.start_link 24, :output
+		state = %State{led1: led1_pid, led2: led2_pid,
+			led3: led3_pid, led4: led4_pid, shutter_button: shutter_pid }
 		Gpio.set_int shutter_pid, :rising
-		IO.puts "Started pin monitor on pin 17"
-		{:ok, {shutter_pid, false }}
+		IO.puts "Started pins"
+		{:ok, {shutter_pid, state }}
 	end
 
-	def handle_info({:gpio_interrupt, 17, :rising}, { shutter_pid, false } ) do
+	def handle_info({:gpio_interrupt, 17, :rising}, state ) when state.busy = false, do
 		IO.puts "handle rising callback from button press."
 		Photobooth.Main.process :booth
-		{:noreply, { shutter_pid, true }}
+		state = %{state | busy: true }
+		{:noreply, state}
 	end
 
-	def handle_info({:gpio_interrupt, pin, state}, { shutter_pid, true } ) do
-		{:noreply, {shutter_pid, true }}
+	def handle_info({:gpio_interrupt, pin, state}, state ) when state.busy = true, do
+		{:noreply, state }
 	end
 
-	def handle_info({:gpio_interrupt, 17, :falling}, { shutter_pid, busy } ) do
+	def handle_info({:gpio_interrupt, 17, :falling}, state ) do
 		IO.puts "handle falling callback from button press."
 		#Photobooth.Main.process :booth
-		{:noreply, { shutter_pid, busy}}
+		{:noreply, state }
 	end
 
-	def handle_cast(:finished, {shutter_pid, busy}) do
-		{:noreply, { shutter_pid, false } }
+	def handle_cast(:finished, state ) do
+		state = %{state | busy: false }
+		{:noreply, state }
 	end
 
-	def terminate(_reason, {shutter_pid}) do
-		Gpio.release shutter_pid
+	def handle_call(:countdown, state) do
+		Gpio.write state.led1 1
+		{:reply, state}
+	end
+
+	defp blink() do
+		Gpio.write state.led1 1
+		Gpio.write state.led2 1
+		Gpio.write state.led3 1
+		Gpio.write state.led4 1
+		:timer.sleep 500
+		Gpio.write state.led1 0
+		Gpio.write state.led2 0
+		Gpio.write state.led3 0
+		Gpio.write state.led4 0
+	end
+
+	def terminate(_reason, state) do
+		Gpio.release state.shutter_button
+		Gpio.release state.led1
+		Gpio.release state.led2
+		Gpio.release state.led3
+		Gpio.release state.led4
 	end
 end
